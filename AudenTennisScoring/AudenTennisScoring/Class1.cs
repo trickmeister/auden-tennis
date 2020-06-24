6cc24ab9e7b2aced19c6d9306b65a80773aa09f8
Mark Trickey <mark.trickmeister@gmail.com>
2020-06-24 11:12:32 +0100

    Created solution and initial test

---
+++ AudenTennisScoring/AudenTennisScoring/Class1.cs
@@ -0,0 +1,7 @@
+﻿namespace AudenTennisScoring
+{
+    public class Class1
+    {
+        public string Name = "Mark";
+    }
+}
---
+++ AudenTennisScoring/AudenTennisScoringTests/TempTest.cs
@@ -0,0 +1,19 @@
+﻿using AudenTennisScoring;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace AudenTennisScoringTests
+{
+    [TestFixture]
+    public class TempTest
+    {
+
+
+        //Used constructor instead of setup as only want DB creation to be done once
+        [Test]
+        public void Class1Test()
+        {
+            new Class1().Name.Should().Be("Mark");
+        }
+    }
+}
